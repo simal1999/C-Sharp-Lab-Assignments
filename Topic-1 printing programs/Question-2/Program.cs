081373246ad1e031bd1cce60a4d93e64f5fdbee7
simal1999 <120905462+simal1999@users.noreply.github.com>
2024-01-05 20:40:36 +0530

    Add files via upload

---
+++ Topic-1 printing programs/Question-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#.Net...");
+            Console.WriteLine("Welcome to Console Application..");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Topic-1 printing programs/Question-3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#.net...");
+            Console.WriteLine("Welcome to C# Application..");
+            Console.WriteLine("Welcome to Sathya..");
+            Console.ReadLine();
+        }
+    }
+}
