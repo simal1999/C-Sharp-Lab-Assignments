41363deb3ab88c3337d66b3bfb951afff8fe1ec3
Simal Gunupudi <120905462+simal1999@users.noreply.github.com>
2024-01-06 00:07:39 +0530

    Add files via upload

---
+++ Topic-3 Type Casting/Question-25/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// write a console program to accept employee number, name, salary and display to the user by using converting?
+            Console.WriteLine("Enter employee number");
+            ulong eno =Convert.ToUInt64(Console.ReadLine());
+            Console.WriteLine("Enter employee name");
+            string name=Console.ReadLine();
+            Console.WriteLine("Enter employee salary");
+            double esal=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Employee number is {eno}");
+            Console.WriteLine($"Employee name is {name}");
+            Console.WriteLine($"Employee salary is {esal}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Topic-3 Type Casting/Question-26/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*write a console program to accept student name and accept student 3 subjects marks i.e m1,m2,m3 and calculate total marks and average marks 
+           finally display to the user total marks , average marks with student name */
+            Console.WriteLine("Enter student name:");
+            string name=Console.ReadLine();
+            Console.WriteLine("Enter first subject marks:");
+            byte m1=Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Enter second subject marks:");
+            byte m2=Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Enter third subject marks:");
+            byte m3=byte.Parse(Console.ReadLine());
+            byte total = (byte)(m1 + m2 + m3);// if we do arthimatic operations on  byte it upgraded to int so we explicitly converts into byte .
+            float average = (total / 3);
+            Console.WriteLine($"{name} total marks is  :{total}");
+            Console.WriteLine($"{name} average marks is:{average}");
+            Console.ReadLine();
+        }
+    }
+}
